2dbd3b783a16add7ab0cf804cdb5b5fea201f278
Uwe Rosner <uwe.rosner@difitec.de>
2018-07-05 16:37:18 +0200

    Improved usage of DataErrorInfo by incorporating the ASP.NET data validations from System.ComponentModel.DataAnnotations.

--- Logic.Ui/DataErrorInfoViewModel.cs
+++ Logic.Ui/DataErrorInfoViewModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 
 namespace Logic.Ui
 {
@@ -13,28 +14,52 @@ namespace Logic.Ui
     #region Fields
 
     private Dictionary<string, string> Errors { get; } = new Dictionary<string, string>();
+    private static List<PropertyInfo> propertyInfos;
 
     #endregion
 
     #region Properties and Commands
 
-    /// <summary>
-    /// Represents the first name of a user
-    /// </summary>
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname must be defined")]
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Firstname must be defined!")]
+    [MaxLength(10, ErrorMessage = "A maximum of 10 characters is allowed!")]
     public string Firstname { get; set; }
 
-    /// <summary>
-    /// Represents the last name of a user
-    /// </summary>
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Lastname must be defined")]
+    // NOTE You can either use the Required or the MinLenght attribute.
+    // They don't work together. This is because Required internal is 
+    // a MinLength=0 attribute, and MinLength can only be added once.
+    [MinLength(4, ErrorMessage = "A minimum of 4 characters must be given!")]
+    [MaxLength(20, ErrorMessage = "A maximum of 20 characters is allowed!")]
     public string Lastname { get; set; }
 
+    public RelayCommand OkCommand { get; private set; }
+
+    /// <summary>
+    /// This is just a little runtime optimization. The PropertyInfos 
+    /// don't change at run time, so they don't needed to be created 
+    /// every time collectErrors() is called. Here they are inizialized
+    /// once and the held in a static list.
+    /// </summary>
+    protected List<PropertyInfo> PropertyInfos
+    {
+      get
+      {
+        if(propertyInfos == null)
+        {
+          propertyInfos = GetType()
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(prop => prop.IsDefined(typeof(RequiredAttribute), true)
+              || prop.IsDefined(typeof(MaxLengthAttribute), true)
+              || prop.IsDefined(typeof(MinLengthAttribute), true))
+            .ToList();
+        }
+
+        return propertyInfos;
+      }
+    }
 
     public bool HasErrors => Errors.Any();
     public bool IsOk => !HasErrors;
-
-    public RelayCommand OkCommand { get; private set; }
 
     #endregion
 
@@ -43,7 +68,8 @@ namespace Logic.Ui
     public DataErrorInfoViewModel()
     {
       OkCommand = new RelayCommand(
-      () => {
+      () =>
+      {
         Trace.TraceInformation("Ok");
         Lastname += Lastname; // TODO remove
       },
@@ -59,15 +85,42 @@ namespace Logic.Ui
     {
       Errors.Clear();
 
-      if (string.IsNullOrEmpty(Firstname))
-      {
-        Errors.Add(nameof(Firstname), "Firstname must be defined");
-      }
+      PropertyInfos.ForEach(
+          prop =>
+          {
+            var currentValue = prop.GetValue(this);
+            var requiredAttr = prop.GetCustomAttribute<RequiredAttribute>();
+            var maxLenAttr = prop.GetCustomAttribute<MaxLengthAttribute>();
+            var minLenAttr = prop.GetCustomAttribute<MinLengthAttribute>();
 
-      if (string.IsNullOrEmpty(Lastname))
-      {
-        Errors.Add(nameof(Lastname), "Lastname must be defined");
-      }
+            if (requiredAttr != null)
+            {
+              if (string.IsNullOrEmpty(currentValue?.ToString() ?? string.Empty))
+              {
+                Errors.Add(prop.Name, requiredAttr.ErrorMessage);
+              }
+            }
+
+            if (maxLenAttr != null)
+            {
+              if ((currentValue?.ToString() ?? string.Empty).Length > maxLenAttr.Length)
+              {
+                Errors.Add(prop.Name, maxLenAttr.ErrorMessage);
+              }
+            }
+
+            if (minLenAttr != null)
+            {
+              if ((currentValue?.ToString() ?? string.Empty).Length < minLenAttr.Length)
+              {
+                Errors.Add(prop.Name, minLenAttr.ErrorMessage);
+              }
+            }
+          });
+
+      // we have to this because the Dictionary does not implement INotifyPropertyChanged            
+      RaisePropertyChanged(nameof(HasErrors));
+      RaisePropertyChanged(nameof(IsOk));
 
       OkCommand.RaiseCanExecuteChanged();
 
