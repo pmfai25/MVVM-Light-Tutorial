993b2a8c07e27f38f27738ed8563d3d548871865
Uwe Rosner <uwe.rosner@difitec.de>
2018-07-06 14:24:35 +0200

    Changed MainWindow view etc to fit the fisrt part of the tutorial pt 4.

---
+++ Logic.Ui/Converters/BoolInverseConverter.cs
@@ -0,0 +1,38 @@
+namespace Logic.Ui.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converts a boolean to its opposite.
+    /// </summary>
+    public class BoolInverseConverter : IValueConverter
+    {
+        #region explicit interfaces
+
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            var transformed = false;
+            if (bool.TryParse(value.ToString(), out transformed))
+            {
+                return !transformed;
+            }
+            throw new ArgumentException("Value is not a bool.");
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
