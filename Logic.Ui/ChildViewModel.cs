5ef1fcefed007e73b959cc7f0be85b4149227558
Uwe Rosner <uwe.rosner@difitec.de>
2018-07-09 09:58:55 +0200

    Changed file encodings to UTF-8 wit *no* BOM

--- Logic.Ui/ChildViewModel.cs
+++ Logic.Ui/ChildViewModel.cs
@@ -3,37 +3,37 @@ using System.Linq;
 
 namespace Logic.Ui
 {
-    using BaseTypes;
+  using BaseTypes;
+
+  /// <summary>
+  /// View logic for a child window.
+  /// </summary>
+  public class ChildViewModel : BaseViewModel
+  {
+    #region constructors and destructors
+
+    public ChildViewModel()
+    {
+      if (IsInDesignMode)
+      {
+        WindowTitle = "ChildWindow (Design)";
+      }
+      else
+      {
+        WindowTitle = "ChildWindow";
+      }
+    }
+
+    #endregion
+
+    #region properties
 
     /// <summary>
-    /// View logic for a child window.
+    /// Some message that is set by the messenger and is defined in the calling
+    /// code from the main view.
     /// </summary>
-    public class ChildViewModel : BaseViewModel
-    {
-        #region constructors and destructors
+    public string MessageFromParent { get; set; }
 
-        public ChildViewModel()
-        {
-            if (IsInDesignMode)
-            {
-                WindowTitle = "ChildWindow (Design)";
-            }
-            else
-            {
-                WindowTitle = "ChildWindow";
-            }
-        }
-
-        #endregion
-
-        #region properties
-
-        /// <summary>
-        /// Some message that is set by the messenger and is defined in the calling
-        /// code from the main view.
-        /// </summary>
-        public string MessageFromParent { get; set; }
-
-        #endregion
-    }
+    #endregion
+  }
 }
