fecb43367fdd1f0d799e537608366c8a7ae14fca
Uwe Rosner <uwe.rosner@difitec.de>
2018-07-12 15:45:08 +0200

    Incorporated a *fast* sort algorithm which sorts directly in the view model instead in the view.

--- Logic.Ui/MainViewModel.cs
+++ Logic.Ui/MainViewModel.cs
@@ -6,6 +6,7 @@ using Logic.Ui.Messages;
 using Logic.Ui.Models;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
@@ -66,9 +67,36 @@ namespace Logic.Ui
 
         Persons = new ObservableCollection<PersonModel>(personList);
         PersonsView = CollectionViewSource.GetDefaultView(Persons) as ListCollectionView;
+
+        // Inform the person model  When the current item of our 
+        // persons view is changed.
         PersonsView.CurrentChanged += (s, e) =>
         {
           RaisePropertyChanged(() => PersonModel);
+        };
+
+        PersonsView.SortDescriptions.Clear();
+        PersonsView.SortDescriptions.Add(new SortDescription(nameof(PersonModel.Firstname), ListSortDirection.Ascending));
+
+        // Hook an event handler for PropertyChanged to all items currently in Persons
+        foreach (var item in Persons)
+        {
+          item.PropertyChanged += PersonsOnPropertyChanged;
+        }
+
+        Persons.CollectionChanged += (s, e) =>
+        {
+          // Hook an event handler for PropertyChanged to all new added items in Persons
+          foreach (INotifyPropertyChanged added in e.NewItems)
+          {
+            added.PropertyChanged += PersonsOnPropertyChanged;
+          }
+
+          // Un-hook the event handler for PropertyChanged to all removed items in Persons
+          foreach (INotifyPropertyChanged removed in e.OldItems)
+          {
+            removed.PropertyChanged -= PersonsOnPropertyChanged;
+          }
         };
 
         OpenChildCommand = new RelayCommand(() => MessengerInstance.Send(new OpenChildWindowMessage("Hello Child!")));
@@ -76,6 +104,38 @@ namespace Logic.Ui
       }
     }
 
+    /// <summary>
+    /// This event handler refreshes the PersonsView.
+    /// 
+    /// So e.g. new sorting is displayed when one item's property 
+    /// changed (see the hooks above).
+    /// 
+    /// In the following situations the refresh is *not* done:
+    /// - Errors occur
+    /// - Only the IsOk status flag has changed
+    /// - While editing is active
+    /// - While adding a new item ist active
+    /// 
+    /// Note: This way of sorting things in the view model is way
+    /// faster than to do it in the DataGrid (view) itself and you 
+    /// have more control how to sort.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void PersonsOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if(e.PropertyName == nameof(PersonModel.HasErrors) || e.PropertyName==nameof(PersonModel.IsOk))
+      {
+        return;
+      }
+
+      if(PersonsView.IsEditingItem || PersonsView.IsAddingNew)
+      {
+        return;
+      }
+
+      PersonsView.Refresh();
+    }
 
     /// <summary>
     /// Indicates the progress.
