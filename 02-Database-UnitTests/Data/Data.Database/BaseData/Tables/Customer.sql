7dea9ccc66329caca836d871d3d3fe72632c32e1
Uwe Rosner <oscottmusic@posteo.de>
2019-02-07 12:57:48 +0100

    Added database / localDB to project; continue at 10:30

---
+++ 02-Database-UnitTests/Data/Data.Database/BaseData/Tables/Customer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [BaseData].[Customer] (
+    [Id]     BIGINT        NOT NULL,
+    [Number] NVARCHAR (10) NOT NULL,
+    [Label]  NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ 02-Database-UnitTests/Data/Data.Database/BaseData/Tables/CustomerGroup.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [BaseData].[CustomerGroup] (
+    [Id]    BIGINT        NOT NULL,
+    [Label] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_CustomerGroup] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
